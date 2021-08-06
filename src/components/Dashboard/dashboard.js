8ebe2fc2a5c8f9b2a1b7a6c76d2583fe7d14c66c
Usenmfon <usenmfonuko@gmail.com>
2021-08-06 13:15:12 +0100

    Fixed bug

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -5,7 +5,7 @@ import { useAuth } from '../../contexts/AuthContext'
 
 
 export default function Dashboard(){
-    const [error, setError] = useState('')
+    const [error] = useState('')
     const { currentUser } = useAuth()
 
     return(
